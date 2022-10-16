a64e7d0f46e408bdc971d14c3e7271a1421974c6
StiLKnoT <stilknot@gmail.com>
2022-10-16 11:36:18 +0500

    changes

--- src/components/Table/DataTables.jsx
+++ src/components/Table/DataTables.jsx
@@ -2,46 +2,44 @@ import React from "react";
 import DataTable from "react-data-table-component";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import './DataTables.css'
-import EditIcon from '@mui/icons-material/Edit';
-import DeleteIcon from '@mui/icons-material/Delete';
-import IosShareIcon from '@mui/icons-material/IosShare';
-
-
-
+import "./DataTables.css";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import IosShareIcon from "@mui/icons-material/IosShare";
 
 function DataTables() {
   const [dataUsers, setDataUsers] = useState([]);
-  const [search, setSearch] = useState('');
-  const [filteredUsers, setFilteredUsers] = useState([])
-  const [deleteUsers, setDeleteUsers] = useState([])
-  const [addUser, setAddUser] = useState(false)
-  const [postUser,setPostUser] = useState([])
-  const [addUsers, setAddUsers] = useState([])
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [role, setRole] = useState('')
-  const [plane, setPlane] = useState('')
-  const [status, setStatus] = useState('')
-  const dataApi = "https://62fc8fecb9e38585cd412f60.mockapi.io/cources"
+  const [search, setSearch] = useState("");
+  const [filteredUsers, setFilteredUsers] = useState([]);
+  const [deleteUsers, setDeleteUsers] = useState([]);
+  const [postUser, setPostUser] = useState([]);
+  const [addUser, setAddUser] = useState(false);
+  const [editUser, setEditUser] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [role, setRole] = useState("");
+  const [plane, setPlane] = useState("");
+  const [status, setStatus] = useState("");
+  const dataApi = `https://62fc8fecb9e38585cd412f60.mockapi.io/cources`;
 
   useEffect(() => {
     getDataUsers();
   }, [deleteUsers, postUser]);
 
   useEffect(() => {
-    const result = dataUsers.filter(user => {
-        return user.name.toLowerCase().match(search.toLowerCase());
-    })
-
-    setFilteredUsers(result)
-    console.log(result)
-
+    const result = dataUsers.filter((user) => {
+      return user.name.toLowerCase().match(search.toLowerCase());
+    });
+    setFilteredUsers(result);
   }, [search]);
 
-
-  
-  
+  const clear = (value) => {
+    setStatus((value = ""));
+    setPlane((value = ""));
+    setRole((value = ""));
+    setEmail((value = ""));
+    setName((value = ""));
+  };
 
 
 
@@ -50,37 +48,56 @@ function DataTables() {
       const response = await axios.get(dataApi);
       setDataUsers(response.data);
       setFilteredUsers(response.data);
-
     } catch (error) {
       console.log(error);
     }
   };
-
 
   const getPostUser = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     try {
-      const resp = await axios.post(dataApi,{name:name, email:email, role:role, plane:plane, status:status});
-      setPostUser(resp.data)
-      console.log(resp.data)
-     
+      const resp = await axios.post(dataApi, {
+        name: name,
+        email: email,
+        role: role,
+        plane: plane,
+        status: status,
+      });
+      setPostUser(resp.data);
+      alert("User added");
+      clear();
 
     } catch (error) {
-      console.log(error);
+      alert("User don't added");
+      clear();
     }
   };
 
- 
+  const getEditUser = async (e, id) => {
+    e.preventDefault();
+    try {
+      const resp = await axios.put(
+        dataApi,
+        { name: name, email: email, role: role, plane: plane, status: status }
+      );
+      setDeleteUsers((prev) => prev.filter((item) => item.id === id));
+      setPostUser(resp.data);
+      alert("User Edited")
+      clear();
 
-  const userDelete = async (id)=>{
-    const respD = await axios.delete(`https://62fc8fecb9e38585cd412f60.mockapi.io/cources/${id}`);
-    setDeleteUsers(prev=>prev.filter((item)=>item.id!==id));    
-    return setDataUsers
-  }
+    } catch (error) {
+      alert("Update error");
+      clear();
+    }
+  };
 
-
-
-  
+  const userDelete = async (id) => {
+    const respD = await axios.delete(
+      `https://62fc8fecb9e38585cd412f60.mockapi.io/cources/${id}`
+    );
+    setDeleteUsers((prev) => prev.filter((item) => item.id !== id));
+    return setDataUsers, alert('User deleted');
+  };
 
   const columns = [
     {
@@ -105,65 +122,188 @@ function DataTables() {
     },
     {
       name: "ACTION",
-      cell: row =><div style={{display:"flex", flexDirection:"row"}}>
-        <button className="btn btn_edit" onClick={()=> alert(row.id)}><EditIcon/></button>
-        {/* <a href="/"> */}
-          <button className="btn btn_delete" onClick={()=> userDelete(row.id)}><DeleteIcon/></button>
-        {/* </a> */}
-        
-      </div>
+      cell: (row) => (
+        <div style={{ display: "flex", flexDirection: "row" }}>
+          <button
+            className="btn btn_edit"
+            onClick={() => setEditUser(!editUser)}
+          >
+            <EditIcon />
+          </button>
+          {editUser ? (
+            <div className="formBody">
+              <div className="AddForm">
+                <span className="editText">Edit User</span>
+
+                <form onSubmit={getEditUser}>
+                  <input
+                    className="input"
+                    type="text"
+                    placeholder="User *"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    required
+                  />
+                  <input
+                    className="input"
+                    type="text"
+                    placeholder="Email *"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                  />
+                  <input
+                    className="input"
+                    type="text"
+                    placeholder="Role *"
+                    value={role}
+                    onChange={(e) => setRole(e.target.value)}
+                    required
+                  />
+                  <input
+                    className="input"
+                    type="text"
+                    placeholder="Plane *"
+                    value={plane}
+                    onChange={(e) => setPlane(e.target.value)}
+                    required
+                  />
+                  <input
+                    className="input"
+                    type="text"
+                    placeholder="Status *"
+                    value={status}
+                    onChange={(e) => setStatus(e.target.value)}
+                    required
+                  />
+                  <div className="formButtons">
+                    <button
+                      className="btn btn_add btnClearAdd"
+                      type="button"
+                      style={{ backgroundColor: "red" }}
+                      onClick={() => setEditUser(!editUser)}
+                    >
+                      Close
+                    </button>
+                    <button className="btn btn_add" type="submit">
+                      Save
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          ) : null}
+          <button className="btn btn_delete" onClick={() => userDelete(row.id)}>
+            <DeleteIcon />
+          </button>
+        </div>
+      ),
     },
   ];
 
-
-
-
-
-
   return (
     <>
-    <div >
-      <DataTable 
-        title="User List"
-        columns={columns}
-        data={filteredUsers}
-        pagination
-        selectableRows
-        selectableRowsHighlight
-        highlightOnHover
-        // actions={
-        //     <button>Export</button>
-        // }
-        subHeader
-        subHeaderComponent={
-            <div className="tableHeader" >
-                <button className="btn btn_export" onClick={()=> alert("Export")}><IosShareIcon/>Export</button>
-                <div className="searchAndAdd">
-                    <input type="text" placeholder="Search Users" className="input searchInput" value={search} onChange={(e) => setSearch(e.target.value)}/>
-                    <button className="btn btn_add" onClick={() => setAddUser(!addUser)}>Add User</button>
-
-                </div>
-          
+      <div>
+        <DataTable
+          title="User List"
+          columns={columns}
+          data={filteredUsers}
+          pagination
+          selectableRows
+          selectableRowsHighlight
+          highlightOnHover
+          subHeader
+          subHeaderComponent={
+            <div className="tableHeader">
+              <button
+                className="btn btn_export"
+                onClick={() => alert("Export")}
+              >
+                <IosShareIcon />
+                Export
+              </button>
+              <div className="searchAndAdd">
+                <input
+                  type="text"
+                  placeholder="Search Users"
+                  className="input searchInput"
+                  value={search}
+                  onChange={(e) => setSearch(e.target.value)}
+                />
+                <button
+                  className="btn btn_add"
+                  onClick={() => setAddUser(!addUser)}
+                >
+                  Add User
+                </button>
+              </div>
             </div>
-        }
-      />
-    </div>
-    {addUser? (<div className="formBody"><div className="AddForm">
-      <form onSubmit={getPostUser}>
-        <input className="input" type="text" placeholder="User" value={name} onChange={(e)=>setName(e.target.value)} required/>
-        <input className="input" type="text" placeholder="Email"value={email} onChange={(e)=>setEmail(e.target.value)} required/>
-        <input className="input" type="text" placeholder="Role" value={role} onChange={(e)=>setRole(e.target.value)} required/>
-        <input className="input" type="text" placeholder="Plane" value={plane} onChange={(e)=>setPlane(e.target.value)} required/>
-        <input className="input" type="text" placeholder="Status" value={status} onChange={(e)=>setStatus(e.target.value)} required/>
-        <div className="formButtons">
-          <button className="btn btn_add" style={{backgroundColor:"green"}} onClick={()=>setAddUser(!addUser)}>Save</button>
+          }
+        />
+      </div>
+      {addUser ? (
+        <div className="formBody">
+          <div className="AddForm">
+            <span className="editText">Add User</span>
 
-          <button className="btn btn_add" type="submit">Add</button>
+            <form onSubmit={getPostUser}>
+              <input
+                className="input"
+                type="text"
+                placeholder="User *"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required
+              />
+              <input
+                className="input"
+                type="text"
+                placeholder="Email *"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+              <input
+                className="input"
+                type="text"
+                placeholder="Role *"
+                value={role}
+                onChange={(e) => setRole(e.target.value)}
+                required
+              />
+              <input
+                className="input"
+                type="text"
+                placeholder="Plane *"
+                value={plane}
+                onChange={(e) => setPlane(e.target.value)}
+                required
+              />
+              <input
+                className="input"
+                type="text"
+                placeholder="Status *"
+                value={status}
+                onChange={(e) => setStatus(e.target.value)}
+                required
+              />
+              <div className="formButtons">
+                <button
+                  className="btn btn_add btnClearAdd"
+                  type="button"
+                  style={{ backgroundColor: "red" }}
+                  onClick={() => setAddUser(!addUser)}
+                >
+                  CLose
+                </button>
+                <button className="btn btn_add btnAdd" type="submit">
+                  Add
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
-      </form>
-
-    </div></div>): null }
-
+      ) : null}
     </>
   );
 }
